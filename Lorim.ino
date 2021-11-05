f29d18c8572a0dbc756b941bfa04482078731159
Kerry Shen <shenzhikai@outlook.com>
2021-11-06 00:22:34 +0800

    Move pin definitions to header file

--- Lorim.ino
+++ Lorim.ino
@@ -4,13 +4,14 @@
 #include "Kbd_8x5_CH450.hpp"
 #include "TaskDispatcher.hpp"
 
-Kbd_8x5_CH450 keyboard(/*sda=*/D1, /*scl=*/D2, /*freq=1E6?*/5000);
-U8G2_DISPLAY_TYPE u8g2(U8G2_R2, /*clock=*/D5, /*data=*/D7, /*cs=*/D8, /*dc=*/D6, /*reset=*/U8X8_PIN_NONE);
+Kbd_8x5_CH450 keyboard(I2C_SDA, I2C_SCL, /*freq=1E6?*/5000);
+U8G2_DISPLAY_TYPE u8g2(U8G2_R2, SPI_CLK, SPI_DATA, SPI_CS, SPI_DC, U8G2_DISPLAY_RESET);
 TaskDispatcher taskDispatcher(u8g2, keyboard);
 
 void saveSettings();
 
 void setup() {
+
     Serial.begin(115200);
     u8g2.begin();
     taskDispatcher.init();
--- Lorim_definitions.hpp
+++ Lorim_definitions.hpp
@@ -4,7 +4,27 @@
 #define GEM_DISABLE_ADAFRUIT_GFX
 #define U8G2_DISPLAY_TYPE U8G2_ST7565_JLX12864_F_4W_SW_SPI
 
+/*
+// esp8266
 #define CH450_INT D0
+#define I2C_SDA D1
+#define I2C_SCL D2
+#define SPI_CLK D5
+#define SPI_DATA D7
+#define SPI_CS D8
+#define SPI_DC D6
+#define U8G2_DISPLAY_RESET U8X8_PIN_NONE
+*/
+
+// esp32
+#define CH450_INT 34
+#define I2C_SDA 21
+#define I2C_SCL 22
+#define SPI_CLK 18
+#define SPI_DATA 23
+#define SPI_CS 5
+#define SPI_DC 19
+#define U8G2_DISPLAY_RESET U8X8_PIN_NONE
 
 // Task IDs (this is not even close to elegance)
 #define DEFAULT_TASK ID_TASKGEM
